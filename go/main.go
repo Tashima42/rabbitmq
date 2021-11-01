de0197610092ec1bcc9972be53fb93e43d4e47d5
Pedro Tashima <pedrotashima42@gmail.com>
2021-11-01 09:29:26 -0300

    added go examples

---
+++ go/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		fmt.Println(err)
+		panic(1)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	fmt.Println(q)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully published ")
+}
